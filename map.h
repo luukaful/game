4e8710fe921514dbb34d43198ada4b79b6c9096a
luukaful <luukaful@gmail.com>
2025-04-27 20:05:18 +0200

    cmake upd.

---
+++ map.h
@@ -0,0 +1,21 @@
+//
+// Created by luukd on 27/04/2025.
+//
+
+#ifndef MAP_H
+#define MAP_H
+
+#include <vector>
+
+
+class Map {
+    std::vector<std::vector<int>> map_data;
+    int breedte;
+    int hoogte;
+public:
+    Map();
+};
+
+
+
+#endif //MAP_H
