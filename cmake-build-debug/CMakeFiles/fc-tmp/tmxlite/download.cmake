c649a175d00a39483f11d1b19deceab1a7ea653b
luukaful <luukaful@gmail.com>
2025-06-28 14:25:57 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/fc-tmp/tmxlite/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for tmxlite")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/marku/CLionProjects/game/cmake-build-debug/CMakeFiles/fc-tmp/tmxlite/tmxlite-gitclone.cmake")
-
-endblock()
